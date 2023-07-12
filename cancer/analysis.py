9f191981dc5131d0d05e5b57db9c025db51b8e65
Austin Vecchio <au.vecchio@gmail.com>
2023-07-11 18:22:15 -0600

    Nearly  working script

--- cancer/analysis.py
+++ cancer/analysis.py
@@ -5,8 +5,9 @@ from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 from sklearn.cluster import AgglomerativeClustering
 from scipy.cluster import hierarchy
 #hierarchy.set_link_color_palette(['c', 'm', 'y', 'k', 'r'])
-rnaseq_df = pd.read_csv("test.rnaseq.txt", sep='\t')
 
+rnaseq_df = pd.read_csv("test.rnaseq.txt", sep='\t')
+'''
 plt.figure(figsize=(10,6))
 link = linkage(rnaseq_df, method='complete', metric='euclidean')
 #c = fcluster(link, t=0.9, criterion='distance')
@@ -23,7 +24,7 @@ plt.title('Dendrogram')
 plt.xlabel('Samples')
 plt.ylabel('Euclidean distances')
 plt.savefig('scipy_dendrogram.png')
-
+'''
 
 
 def plot_dendrogram(model, **kwargs):
@@ -65,19 +66,26 @@ def plot_dendrogram(model, **kwargs):
 #print(clustering.labels_)
 
 # setting distance_threshold=0 ensures we compute the full tree.
-model = AgglomerativeClustering(n_clusters=4, affinity="euclidean", linkage="complete", compute_distances=True) #, distance_threshold=None)
+
+
+model = AgglomerativeClustering(n_clusters=4, metric="euclidean", linkage="complete", compute_distances=True, distance_threshold=None)
 
 model = model.fit(rnaseq_df.transpose())
-#print(model.feature_names_in_)
-print(model.labels_)
+#print(rnaseq_df.shape)
+#print(len(model.feature_names_in_))
+#print(len(model.labels_))
 group_map = {}
 samples = rnaseq_df.columns
+n_samples = len(samples)
 labels = model.labels_
+n_labels = len(labels)
+print(f'Samples: {n_samples}')
+print(f'Labels: {n_labels}')
 for index, sample in enumerate(samples):
     group_map[sample] = labels[index]
 
-print(group_map)
-
+#print(group_map)
+'''
 plt.figure(figsize=(10,6))
 plt.title("Hierarchical Clustering Dendrogram")
 # plot the top three levels of the dendrogram
@@ -85,7 +93,7 @@ plot_dendrogram(model)
 #print(model.children_)
 plt.xlabel("Number of points in node (or index of point if no parenthesis).")
 plt.savefig('scikitlearn_dendrogram.png')
-
+'''
 
 #print(group_map)
 
@@ -95,7 +103,7 @@ end = 37884911
 #chr17:37,844,347-37,884,911
 group_count = {}
 for label in labels:
-    group_count[str(label)] = 0
+    group_count[label] = 0
 
 cnvs_df = pd.read_csv("test.CNV.genomicSegment.txt", sep='\t')
 for record in cnvs_df.to_dict('records'):
@@ -103,20 +111,30 @@ for record in cnvs_df.to_dict('records'):
         if (record['CNV'] > 0):
             sample = record['Sample']
             group_id = group_map[sample]
-            group_count[str(label)] += 1
+            group_count[group_id] += 1
 print(group_count)
-
+print('===========')
 mutations_df = pd.read_csv("test.mutation.txt", sep='\t')
+print(len(mutations_df))
+filtered_mutations_df = mutations_df[mutations_df['effect'] != 'Silent']
+print(len(filtered_mutations_df))
 
 group_gene_count = {}
 for label in labels:
     group_gene_count[label] = {}
-for record in mutations_df.to_dict('records'):
+for record in filtered_mutations_df.to_dict('records'):
     sample = record['sample']
     gene = record['gene']
-
     group_id = group_map[sample]
-    if gene not in group_gene_count[label]:
-        group_gene_count[label][gene] = 0
-    group_gene_count[label][gene] += 1
+    if gene not in group_gene_count[group_id]:
+        group_gene_count[group_id][gene] = 0
+    group_gene_count[group_id][gene] += 1
 #print(group_gene_count)
+
+for group in group_gene_count:
+    genes_and_counts = []
+    for gene in group_gene_count[group]:
+        gene_count = group_gene_count[group][gene]
+        genes_and_counts.append((gene, gene_count))
+    genes_and_counts.sort(key=lambda a: a[1], reverse=True)
+    print(group, genes_and_counts[0:5])
